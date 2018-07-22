fix average

movi/dqn/feature_constructor.py
@@ -16,7 +16,7 @@ class FeatureConstructor(object):
         self.state_space = [(x, y) for x in range(MAP_WIDTH) for y in range(MAP_HEIGHT) if self.reachable_map[x, y] == 1]
         self.tt_map = self.load_tt_map()
         if FLAGS.use_average:
-            self.D_out = self.D_in = np.ones(MAP_WIDTH, MAP_HEIGHT) / (L ** 2)
+            self.D_out = self.D_in = np.ones((MAP_WIDTH, MAP_HEIGHT)) / (L ** 2)
         else:
             self.D_out, self.D_in = self.build_diffusion_filter()
 

movi/experiment.py
@@ -34,9 +34,10 @@ class Experiment(object):
         self.simulator.dispatch_vehicles(d_commands)
 
         net_v = vehicles[vehicles.status != vehicle_status_codes.OFF_DUTY]
-        summary = "{:d}, {:d}, {:d}, {:d}, {:d}, {:d}".format(
+        average_wt = np.mean([command['duration'] for command in m_commands]).astype(int)
+        summary = "{:d}, {:d}, {:d}, {:d}, {:d}, {:d}, {:d}".format(
             current_time, len(net_v), len(net_v[net_v.status == vehicle_status_codes.OCCUPIED]),
-            len(requests), len(m_commands), len(d_commands)
+            len(requests), len(m_commands), len(d_commands), average_wt
         )
         sim_logger.log_summary(summary)
 