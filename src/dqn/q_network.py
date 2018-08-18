c3ded953d6fd68db17d474a56583309b4d3a37b7
織田拓磨 <misteroda0220@gmail.com>
2018-08-18 15:34:43 +0900

    no-dsql

--- src/dqn/q_network.py
+++ src/dqn/q_network.py
@@ -111,10 +111,11 @@ class FittingDeepQNetwork(DeepQNetwork):
 
     def compute_target_value(self, s):
         Q = self.compute_target_q_values(s)
-        amax = np.argmax(self.compute_q_values(s))
-        V = Q[amax]
         if FLAGS.alpha > 0:
-            V += FLAGS.alpha * np.log(np.exp((Q - Q.max()) / FLAGS.alpha).sum())
+            V = Q.max() + FLAGS.alpha * np.log(np.exp((Q - Q.max()) / FLAGS.alpha).sum())
+        else:
+            amax = np.argmax(self.compute_q_values(s))
+            V = Q[amax]
         return V
 
 
